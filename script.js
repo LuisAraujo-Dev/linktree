8beedbbba1b7928fe72ff76276552a0aa73eda9a
LuisAraujo-Dev <luisaraujo.dev@gmail.com>
2024-02-11 12:40:21 -0300

    initial

---
+++ script.js
@@ -0,0 +1,24 @@
+function toggleMode() {
+    const html = document.documentElement
+  
+    /*if (html.classList.contains("light")) {
+      html.classList.remove("light")
+    } else {
+      html.classList.add("light")
+    }*/
+  
+    html.classList.toggle("light")
+  
+    // pegar a tag imag
+    const img = document.querySelector("#profile img")
+    
+    // substituit a imag
+    if (html.classList.contains("light")) {
+      // se tiver light mode, adicionar a imagem light
+      img.setAttribute("src", "./asset/avatarLIGHT.png")
+    } else {
+      // se nao tiver light mode, manter a imagem normal
+      img.setAttribute("src", "./asset/avatar.png")
+    }
+  }
+  
