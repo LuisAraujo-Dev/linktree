0c1e25b12da694b2b51beb2b48cc89654f16f499
LuisAraujo-Dev <luisaraujo.dev@gmail.com>
2025-07-01 19:35:00 -0300

    add: imagem dos produtos

--- src/data/products.ts
+++ src/data/products.ts
@@ -5,14 +5,14 @@ export const products = [
     id: 1,
     title: "Nome do Produto 1",
     content: "Descrição breve do produto.",
-    image: "/images/produto1.png",
+    image: "/images/imagem do seu produto.png",
     link: "https://wa.me/55999999999?text=Olá, tenho interesse no Produto 1",
   },
   {
     id: 2,
     title: "Nome do Produto 2",
     content: "Descrição breve do produto.",
-    image: "/images/produto2.png",
+    image: "/images/imagem do seu produto.png",
     link: "https://wa.me/55999999999?text=Olá, tenho interesse no Produto 2",
   },
 ];
