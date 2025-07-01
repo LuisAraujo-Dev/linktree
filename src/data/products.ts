577e5354b2b0e323b061609976686940cdae78b5
LuisAraujo-Dev <luisaraujo.dev@gmail.com>
2025-07-01 19:12:10 -0300

    feat: criado data/product.ts

---
+++ src/data/products.ts
@@ -0,0 +1,18 @@
+
+
+export const products = [
+  {
+    id: 1,
+    name: "Nome do Produto 1",
+    description: "Descrição breve do produto.",
+    image: "/images/produto1.png",
+    link: "https://wa.me/55999999999?text=Olá, tenho interesse no Produto 1",
+  },
+  {
+    id: 2,
+    name: "Nome do Produto 2",
+    description: "Descrição breve do produto.",
+    image: "/images/produto2.png",
+    link: "https://wa.me/55999999999?text=Olá, tenho interesse no Produto 2",
+  },
+];
